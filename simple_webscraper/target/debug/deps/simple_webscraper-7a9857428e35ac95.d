4f446291a969f7c7ac58d2195da218d1c23aa1ef
kapllanajdev <143256388+KaplanXYZ@users.noreply.github.com>
2024-09-27 18:40:42 +0200

    Initial commit

---
+++ simple_webscraper/target/debug/deps/simple_webscraper-7a9857428e35ac95.d
@@ -0,0 +1,5 @@
+/Users/alexanderkapllanaj/webscraper_rust/simple_webscraper/target/debug/deps/simple_webscraper-7a9857428e35ac95: src/main.rs
+
+/Users/alexanderkapllanaj/webscraper_rust/simple_webscraper/target/debug/deps/simple_webscraper-7a9857428e35ac95.d: src/main.rs
+
+src/main.rs:
---
+++ simple_webscraper/target/debug/simple_webscraper.d
@@ -0,0 +1,1 @@
+/Users/alexanderkapllanaj/webscraper_rust/simple_webscraper/target/debug/simple_webscraper: /Users/alexanderkapllanaj/webscraper_rust/simple_webscraper/src/main.rs
